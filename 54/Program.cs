a072e453f276e1764360ab1cc40cc9ec552fb515
Davidenko Ilia <Ilia_d@outlook.com>
2022-05-17 19:58:34 +0300

    Задачи 54 56

---
+++ 54/Program.cs
@@ -0,0 +1,55 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] randArray = GetRandomArray(5, 5, 0, 20);
+PrintArray(randArray);
+SortArray(randArray);
+Console.WriteLine();
+PrintArray(randArray);
+
+
+int[,] GetRandomArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] SortArray(int[,] array)
+{
+    int emptyNumber;
+    for (int k = 0; k < array.GetLength(0); k++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1) - 1; j++)
+            {
+                if (array[i, j] < array[i, j + 1])
+                {
+                    emptyNumber = array[i, j];
+                    array[i, j] = array[i, j + 1];
+                    array[i, j + 1] = emptyNumber;
+                }
+            }
+        }
+    }
+    return array;
+}
---
+++ 56/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов. Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] newArray = GetRandomArray(4, 5, 0, 10);
+PrintArray(newArray);
+SumRow(newArray);
+
+
+int[,] GetRandomArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void SumRow(int[,] array)
+{
+    int minSum = int.MaxValue;
+    int indexMinSum = 0;
+    int sum;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        Console.WriteLine($"Сумма в {i} строке {sum}");
+        if (sum < minSum)
+        {
+            minSum = sum;
+            indexMinSum = i;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Минимальная сумма {minSum} находится в {indexMinSum} строке");
+}
